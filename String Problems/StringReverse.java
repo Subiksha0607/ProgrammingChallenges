9619f4afc33d5cd7613b1dc202c06e5e0db702d8
Subiksha0607 <s.subikshasrinivasan@gmail.com>
2024-11-05 21:37:30 +0530

    Add files via upload

--- String Problems/StringReverse.java
+++ String Problems/StringReverse.java
@@ -1,25 +1,28 @@
 package problem.solving;
 import java.lang.String;
 import java.util.Scanner;
+
 public class StringReverse {
-	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		String sen=scan.next();
-		System.out.println((sentenceRev(sen)));	
-		scan.close();
-	}
-	static String sentenceRev(String str) {
+
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	String sen=scan.next();
+	System.out.println((sentenceRev(sen)));	
+	scan.close();
+}
+static String sentenceRev(String str) {
 	String rev="";
 	String[] sarr=str.split(" ");
 	for(String x:sarr) {
-    	rev=rev+revStr(x)+" ";
+		rev=rev+revStr(x)+" ";
 	}
 	return rev;
-	}
-	static String revStr(String ostr) {
-        String rev = "";   
-        for(int i=0;i<ostr.length();i++)
-        	rev=ostr.substring(i,i+1)+rev;
+}
+
+static String revStr(String ostr) {
+     String rev = "";   
+     for(int i=0;i<ostr.length();i++)
+        rev=ostr.substring(i,i+1)+rev;
         return rev;
-    }
+}
 }
